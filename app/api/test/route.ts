6bb68b30d75a5d92bfa6723083a62e40e417ec22
rodinshrestha <rodinshrestha@gmail.com>
2024-06-04 13:41:20 +0545

    fix: api issue

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,5 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
 const getRandomInteger = (min: number, max: number) => {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -7,14 +5,8 @@ const getRandomInteger = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-export function GET(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    // res.status(200).json({ result: getRandomInteger(1, 10) })
-    //  return new Response(`${getRandomInteger(1, 10)}`, {status: 200})
-    return new Response(JSON.stringify({ data: getRandomInteger(1, 10) }), {
-      headers: { 'Content-Type': 'application/json' },
-    });
-  } catch (err) {
-    res.status(500).json({ error: 'failed to load data' });
-  }
+export function GET() {
+  return new Response(JSON.stringify({ data: getRandomInteger(1, 10) }), {
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
--- components/cache-data.tsx
+++ components/cache-data.tsx
@@ -2,7 +2,7 @@ import { headers } from 'next/headers';
 import React from 'react';
 
 const CacheData = async () => {
-  const data = await fetch('http://localhost:3000/api/test', {
+  const data = await fetch('https://next-cache-test-nine.vercel.app/api/test', {
     headers: { 'Content-Type': 'application/json' },
   })
     .then(async (res) => {
